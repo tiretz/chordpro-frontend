68e41614a8b0cdaa4dfeff780d13b80f1f15845a
Tim Retzlaff <retzlaff-tim@gmx.de>
2023-07-26 18:25:06 +0200

    Add empty editor component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
