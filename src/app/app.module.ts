0f33ca4845017fd8feb944cfacd6d2d73c81eb9c
Tim Retzlaff <retzlaff-tim@gmx.de>
2023-07-27 16:21:07 +0200

    Initial monaco editor integration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,39 @@ import { EditorComponent } from './editor/editor.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MonacoEditorModule, NgxMonacoEditorConfig } from 'ngx-monaco-editor-v2';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+const monacoConfig: NgxMonacoEditorConfig = {
+	defaultOptions: {
+		theme: 'vs-dark',
+		language: 'javascript',
+		scrollBeyondLastLine: false,
+		minimap: { enabled: false },
+		automaticLayout: true,
+		fixedOverflowWidgets: true,
+		wordWrap: 'on',
+    	wrappingStrategy: 'advanced',
+	},
+	onMonacoLoad: () => { console.log((<any>window).monaco); }
+};
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    EditorComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		EditorComponent,
+		FooterComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MonacoEditorModule.forRoot(monacoConfig),
+		MatToolbarModule
+	],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
-  selector: 'app-editor',
-  templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.css']
+	selector: 'app-editor',
+	templateUrl: './editor.component.html',
+	styleUrls: ['./editor.component.css']
 })
 export class EditorComponent {
 
+	editorOptions = {};
+
+	code: string = 'function x() {\nconsole.log("Hello world!");\n}';
+
+	onInit(editor: any) {
+		
+		// set default template
+		editor.setValue(this.code)
+	}
 }
